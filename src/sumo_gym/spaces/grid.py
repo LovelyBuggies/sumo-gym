1a44c243e89bb8c08c3e135ad4d7214d36091e27
N!no <ninomyemail@gmail.com>
2021-11-14 14:35:05 -0500

    fix:

--- src/sumo_gym/spaces/grid.py
+++ src/sumo_gym/spaces/grid.py
@@ -58,9 +58,13 @@ class GridSpace(gym.spaces.Space):
                         loc = grid_utils.one_step_to_destination(self.vertices, self.edges, self.locations[i], self.charging_stations[ncs][0])
                         samples[i] = (-1, ncs, loc) if loc == self.charging_stations[ncs] else (-1, -1, loc)
                     else:
-                        dmd_idx = random.sample(set(range(len(self.demand))) - self.responded, 1)[0]
-                        self.responded.add(dmd_idx)
-                        loc = grid_utils.one_step_to_destination(self.vertices, self.edges, self.locations[i], self.demand[dmd_idx][0])
-                        samples[i] = (dmd_idx, -1, loc) if loc == self.demand[dmd_idx][0] else (-1, -1, loc)
+                        available_dmd = set(range(len(self.demand))) - self.responded
+                        if len(available_dmd):
+                            dmd_idx = random.sample(available_dmd, 1)[0]
+                            self.responded.add(dmd_idx)
+                            loc = grid_utils.one_step_to_destination(self.vertices, self.edges, self.locations[i], self.demand[dmd_idx][0])
+                            samples[i] = (dmd_idx, -1, loc) if loc == self.demand[dmd_idx][0] else (-1, -1, loc)
+                        else:
+                            samples[i] = (-1, -1, self.locations[i])
 
         return samples
