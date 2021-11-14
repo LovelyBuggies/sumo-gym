2ef253f83ae555cebb7e16f697aa342806a71930
N!no <ninomyemail@gmail.com>
2021-11-14 17:10:49 -0500

    Update make-fmpenv-data-simple.py

--- tutorial/make-fmpenv-data-simple.py
+++ tutorial/make-fmpenv-data-simple.py
@@ -60,7 +60,7 @@ env.render()
 
 for i_episode in range(3):
     observation = env.reset()
-    for t in range(10):
+    for t in range(100):
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         if done:
