066bec9c74bb04bb464bc80fe560d816b6a7e1fe
N!no <ninomyemail@gmail.com>
2021-11-14 10:51:40 -0500

    fix: true->idx

--- src/sumo_gym/spaces/grid.py
+++ src/sumo_gym/spaces/grid.py
@@ -33,17 +33,18 @@ class GridSpace(gym.spaces.Space):
         for i in range(n_vehicle):
             if self.is_loading[i] != -1:
                 loc = self.demand[self.is_loading][1]  # one step towards this direction
-                samples[i] = (False, False, loc) if loc == self.demand[self.is_loading][1] else (True, False, loc)
+                samples[i] = (-1, -1, loc) if loc == self.demand[self.is_loading][1] else (self.is_loading, -1, loc)
             else:
                 if self.is_charging[i] != -1:
-                    samples[i] = (False, True, self.charging_stations[self.is_charging[i]])
+                    # judge whether finished
+                    samples[i] = (-1, True, self.charging_stations[self.is_charging[i]])
                 else:
                     ncs, battery_threshold = find_the_nearest_charging_station_and_its_distance()  # one step towards
                     possibility_of_togo_charge = -log(self.batteries[i] - battery_threshold)
                     if self.batteries[i] < battery_threshold or np.random.random() < possibility_of_togo_charge:
                         loc = self.charging_stations[ncs] # one step towards this direction
-                        samples[i] = (False, True, ncs) if loc == self.charging_stations[ncs] else (False, False, ncs)
+                        samples[i] = (-1, ncs, loc) if loc == self.charging_stations[ncs] else (-1, -1, loc)
                     else:
                         dmd_idx = find_a_request_to_respond()
                         loc = self.demand[dmd_idx]  # one step towards this direction
-                        samples[i] = (True, False, loc) if loc == self.demand[dmd_idx] else (False, False, loc)
+                        samples[i] = (dmd_idx, -1, loc) if loc == self.demand[dmd_idx] else (-1, -1, loc)
