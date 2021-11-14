b67909e76387e9b49e3e1269cc16321e63e0245c
N!no <ninomyemail@gmail.com>
2021-11-14 12:13:18 -0500

    Update fmp.py

--- src/sumo_gym/envs/fmp.py
+++ src/sumo_gym/envs/fmp.py
@@ -140,10 +140,8 @@ class FMPEnv(gym.Env):
             prev_location = self.locations[i]
             prev_is_loading = self.is_loading[i]
             prev_is_charging = self.is_charging[i]
-            self.locations[i] = actions[i][2]
+            self.is_loading[i], self.is_charging[i], self.locations[i] = actions[i]
             self.batteries -= self.locations[i] - prev_location
-            self.is_loading[i] = actions[i][0]
-            self.is_charging[i] = actions[i][1]
             self.rewards[i] -= func(self.locations[i] - prev_location) # should be based on current battery, if low, f is high
             if prev_is_loading != -1 and self.is_loading[i] == -1:
             self.rewards[i] += grid_utils.get_hot_spot_weight(self.fmp.vertices, self.fmp.edges, self.locations[i], self.fmp.demand[prev_is_loading][0]) \
