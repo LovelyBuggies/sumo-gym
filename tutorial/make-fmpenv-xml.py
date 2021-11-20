c4f50c78c7a96018840234fafcf9649a30355561
yunhaow <yw3736@columbia.edu>
2021-11-20 14:02:07 -0500

    test case for fmp sumo xml initialization

---
+++ tutorial/make-fmpenv-xml.py
@@ -0,0 +1,24 @@
+import numpy as np
+import gym
+import sumo_gym
+from sumo_gym.envs.vrp import VRP
+import matplotlib
+import matplotlib.pyplot as plt
+
+env = gym.make(
+    "FMP-v0",
+    net_xml_file_path="assets/data/network.net.xml",
+    demand_xml_file_path="assets/data/demand.rou.xml",
+)
+
+env.render()
+for i_episode in range(1):
+    observation = env.reset()
+    for t in range(150):
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps.\n".format(t + 1))
+            break
+
+env.close()
