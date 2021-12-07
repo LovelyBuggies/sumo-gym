181917d55dd5828ceff38ede5606cf1e6ea04e83
N!no <ninomyemail@gmail.com>
2021-12-07 18:57:02 -0500

    feat: set up version 0.2.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ extras_require["all"] = sorted(set(sum(extras_require.values(), [])))
 
 setuptools.setup(
     name="sumo_gym",
-    version="0.1.0",
+    version="0.2.0",
     author="Shuo Liu",
     author_email="ninomyemail@gmail.com",
     description="OpenAI-gym like toolkit for developing and comparing reinforcement learning algorithms on SUMO",
