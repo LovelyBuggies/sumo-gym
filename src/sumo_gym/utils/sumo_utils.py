574a5b95a703aeb68b1ef02caf2b899bceb9bf7d
yunhaow <yw3736@columbia.edu>
2021-12-07 14:10:59 -0500

    fix charging station resume issue

--- src/sumo_gym/utils/sumo_utils.py
+++ src/sumo_gym/utils/sumo_utils.py
@@ -133,10 +133,12 @@ class SumoRender:
                 )
                 if (
                     self.routes[i][-1] != self.routes[i][-2]
-                ):  # handle the case for stopping at CS and then resume
+                ): # handle the case for stopping at CS and then resume
                     traci.vehicle.setRoute(
                         vehID=vehicle_id, edgeList=self.routes[i][-2:]
                     )
+                else:
+                    edge_id = actual_edge_id
                 traci.vehicle.setStop(
                     vehID=vehicle_id,
                     edgeID=actual_edge_id,
