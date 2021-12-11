aa2d7ace0f859cb287d60dbead72767d55731483
nmauskar <nmauskar@ucsd.edu>
2021-12-11 15:38:24 -0500

    Added Stanford to map list

---
+++ tutorials/fmp-stanford.py
@@ -0,0 +1,24 @@
+import sys
+import gym
+import sumo_gym
+
+if __name__ == "__main__":
+    env = gym.make(
+        "FMP-v0",
+        sumo_configuration_path=sys.argv[sys.argv.index("--sumo-config-path") + 1],
+        net_xml_file_path="assets/data/stanford/osm.net.xml",
+        demand_xml_file_path="assets/data/stanford/osm.rou.xml",
+        additional_xml_file_path="assets/data/stanford/osm.cs.add.xml",
+    )
+
+    for i_episode in range(1):
+        observation = env.reset()
+        for t in range(3000):
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            env.render()
+            if done:
+                print("Episode finished after {} timesteps.\n".format(t + 1))
+                break
+
+    env.close()
