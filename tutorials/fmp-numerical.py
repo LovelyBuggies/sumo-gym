4615fcd35f9651dd0ab7229121b975f4796132a1
N!no <ninomyemail@gmail.com>
2021-12-28 00:47:01 -0500

    feat: finish spaces

--- tutorials/fmp-numerical.py
+++ tutorials/fmp-numerical.py
@@ -240,7 +240,7 @@ for i_episode in range(1):
     for t in range(150):
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        print(observation["Takes_action"])
+        # print(observation["Takes_action"])
         if done:
             print("Episode finished after {} timesteps.\n".format(t + 1))
             break
