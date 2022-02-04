73d7661007394ebefcaa0334fd92cd3aa5c476bd
N!no <ninomyemail@gmail.com>
2022-02-04 18:13:19 -0500

    Update sb3.py

--- baselines/sb3.py
+++ baselines/sb3.py
@@ -284,7 +284,7 @@ class MADQN(object):
                 observation, reward, done, info = env.last()
                 prev_state = None if len(self.replay_buffer[agent]) == 0 else self.replay_buffer[agent][-1][2]
                 cum_reward[agent] += reward
-                if prev_state != observation[:3] and observation[-1] != None and prev_state != None:
+                if prev_state != observation[:3]:
                     self.replay_buffer[agent].push(
                         (
                             prev_state,
