0a0486463a1a926dd4e076be98a14a03c94caee7
yunhaow <yw3736@columbia.edu>
2022-03-05 15:01:58 -0800

    plot

--- baselines/generate_plot.py
+++ baselines/generate_plot.py
@@ -4,9 +4,18 @@ import json
 loss_dict = json.load(open('loss.json', 'r'))
 episode = [key for key, _ in loss_dict.items()]
 v0_loss = [value["v0"] for _, value in loss_dict.items()]
-plt.grid(True)
+v1_loss = [value["v1"] for _, value in loss_dict.items()]
+v2_loss = [value["v2"] for _, value in loss_dict.items()]
 
-plt.plot(episode, v0_loss, color='maroon', marker='o')
+plt.plot(episode[2:], v0_loss[2:], color='aquamarine')
+plt.xlabel('episode')
+plt.ylabel('loss')
+
+plt.plot(episode[2:], v1_loss[2:], color='cornflowerblue')
+plt.xlabel('episode')
+plt.ylabel('loss')
+
+plt.plot(episode[2:], v2_loss[2:], color='wheat')
 plt.xlabel('episode')
 plt.ylabel('loss')
 
