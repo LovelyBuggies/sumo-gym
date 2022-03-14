f4fe78967b7c2f232561fa34f799f6f86a78b89b
N!no <ninomyemail@gmail.com>
2022-03-14 19:45:13 -0400

    refactory: fig gernerator

--- baselines/generate_plot.py
+++ baselines/generate_plot.py
@@ -38,22 +38,20 @@ def plot_loss(lower_loss_file, upper_loss_file, upper_loss_key, upper_smooth, st
 
     upper_loss_dict = json.load(open(upper_loss_file, "r"))
     upper_loss = np.array([value[upper_loss_key] for _, value in upper_loss_dict.items()][start_index:end_index])
-    
+
+    plt.figure(figsize=(12, 6))
     plt.rcParams["font.size"] = '14'
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
-    y1 = ax1.plot(episode[truncate_prefix:], lower_loss[truncate_prefix:], color="gold", label="lower level loss")
+    y1 = ax1.plot(episode[truncate_prefix:], lower_loss[truncate_prefix:], color="navy", ls='--', lw=4, label="lower level loss")
 
     upper_loss_smooth = savitzky_golay(upper_loss, 51, 3) if upper_smooth else upper_loss
-    y2 = ax2.plot(episode[truncate_prefix:], upper_loss_smooth[truncate_prefix:], color="dodgerblue", label="upper level loss")
-
-    plt.xlabel("episode")
+    y2 = ax2.plot(episode[truncate_prefix:], upper_loss_smooth[truncate_prefix:], color="indianred", ls='-', lw=4, label="upper level loss")
 
     lns = y1 + y2
     labs = [l.get_label() for l in lns]
-    ax1.legend(lns, labs, loc="upper right") 
-    plt.show()
+    ax1.legend(lns, labs, loc="upper right")
 
 
 def plot_reward(lower_reward_file, upper_reward_file, upper_reward_key, smooth, start_index, end_index, scale=False):
@@ -63,39 +61,30 @@ def plot_reward(lower_reward_file, upper_reward_file, upper_reward_key, smooth, 
 
     upper_reward_dict = json.load(open(upper_reward_file, "r"))
     upper_reward = np.array([value[upper_reward_key] for _, value in upper_reward_dict.items()][start_index:end_index])
-    
+
+    plt.figure(figsize=(12, 6))
     plt.rcParams["font.size"] = '14'
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
     lower_reward_smooth = savitzky_golay(lower_reward, 51, 5) if smooth else savitzky_golay(lower_reward, 31, 1)
-    y1 = ax1.plot(episode, lower_reward_smooth, color="gold", label="lower level reward")
+    y1 = ax1.plot(episode, lower_reward_smooth, color="navy", ls='--', lw=4, label="lower level reward")
 
     upper_reward_smooth = savitzky_golay(upper_reward, 51, 5) if smooth else savitzky_golay(upper_reward, 11, 1)  
-    y2 = ax2.plot(episode, upper_reward_smooth, color="dodgerblue", label="upper level reward")
+    y2 = ax2.plot(episode, upper_reward_smooth, color="indianred", ls='-', lw=4, label="upper level reward")
 
     plt.xlabel("episode")
 
     lns = y1 + y2
     labs = [l.get_label() for l in lns]
-    ax1.legend(lns, labs, loc="upper right") 
-    plt.show()
+    ax1.legend(lns, labs, loc="upper right")
 
+plot_loss("jumbo-lower-loss.json", "jumbo-upper-loss.json", 'v0', True, 50, 129, 10)
+plt.savefig("jumbo-loss.pdf")
+plot_reward("jumbo-lower-reward.json", "jumbo-upper-reward.json", '0', True, 0, 57, True)
+plt.savefig("jumbo-reward.pdf")
+plot_loss("cosmos-lower-loss.json", "cosmos-upper-loss.json", '-5', False, 0, 17, 4)
+plt.savefig("cosmos-loss.pdf")
+plot_reward("cosmos-lower-reward.json", "cosmos-upper-reward.json", '-2', False, 0, 17)
+plt.savefig("cosmos-reward.pdf")
 
-def plot_jumbo_loss():
-    plot_loss("jumbo-lower-loss.json", "jumbo-upper-loss.json", 'v0', True, 50, 129, 10)
-
-def plot_jumbo_reward():
-    plot_reward("jumbo-lower-reward.json", "jumbo-upper-reward.json", '0', True, 0, 57, True)
-
-def plot_cosmos_loss():
-    plot_loss("cosmos-lower-loss.json", "cosmos-upper-loss.json", '-5', False, 0, 17, 4)
-
-def plot_cosmos_reward():
-    plot_reward("cosmos-lower-reward.json", "cosmos-upper-reward.json", '-2', False, 0, 17)
-
-
-plot_jumbo_loss()
-plot_jumbo_reward()
-plot_cosmos_loss()
-plot_cosmos_reward()
